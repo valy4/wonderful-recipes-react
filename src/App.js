92ff7743e6cb2608b6fe3254a069fa33cb14d4ac
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-04-27 15:21:30 +0100

    react intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react"
 function App() {
+  const [hidden, setHidden] = useState(false)
+  const [counter, setCounter] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="title">Wonderful Recipes</h1>
+        <button onClick={(event) => (setHidden(true))}>hide me</button>
+        <button onClick={(event) => (setHidden(false))}>show me</button>
+        {hidden ? <p>hidden</p> : <p>i am not hidden</p>}
+        <h2>{counter}</h2>
+        <button onClick={(event) => (setCounter(counter + 1))}>increment</button>
+        <button onClick={(event) => (setCounter(counter - 1))}>decrement</button>
+
+      </div>
+
     </div>
   );
 }
