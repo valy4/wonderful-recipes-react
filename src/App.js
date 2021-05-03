3cc56e14960842c9c0f0fbe11082e7ddaa3b0c41
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-05-03 09:00:33 +0100

    recipes card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import react from "react"
 import './App.css';
 import { useState } from "react"
+
+const callApiKey = "0bfd9699a1f041569f9c334fa5e31134"
 function App() {
-  const [hidden, setHidden] = useState(false)
-  const [counter, setCounter] = useState(0)
+  const [recipes, setRecipes] = useState([]) // keep stuff in memory for us to use it
+  const [search, setSearch] = useState("")
+  function getRecipes(event) {
+    console.log(event)
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?query=${search}&addRecipeInformation=true&apiKey=${callApiKey}`
+    )
+      .then((response) => response.json())
+
+      .then((data) => { setRecipes(data.results) })
+    console.log(recipes)
+  }
   return (
-    <div className="App">
-      <div className="container">
-        <h1 className="title">Wonderful Recipes</h1>
-        <button onClick={(event) => (setHidden(true))}>hide me</button>
-        <button onClick={(event) => (setHidden(false))}>show me</button>
-        {hidden ? <p>hidden</p> : <p>i am not hidden</p>}
-        <h2>{counter}</h2>
-        <button onClick={(event) => (setCounter(counter + 1))}>increment</button>
-        <button onClick={(event) => (setCounter(counter - 1))}>decrement</button>
+    <div className="container">
+      <h1 className="title">Wonderful Recipes</h1>
+      <input id="search-bar" onChange={event => (setSearch(event.target.value))} type="text" placeholder="Search the best recipe..." />
+      <button className="recipe-btn" onClick={getRecipes}>Search</button>
 
-      </div>
-
-    </div>
+      {recipes.map(recipe => <div className="recipe-card">
+        <h4>{recipe.title}</h4>
+        <img src={recipe.image} />
+        <button className="recipe-btn">{recipe.button}See full recipe</button>
+      </div>)}
+    </div >
   );
 }
 
